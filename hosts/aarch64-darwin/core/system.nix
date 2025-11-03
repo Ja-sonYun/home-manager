a429c66e501ef36ee1d54ed2ddc2655a3b22d86f
Ja-sonYun <killa30867@gmail.com>
2025-11-03 18:29:16 +0900

    Chore

--- hosts/aarch64-darwin/core/system.nix
+++ hosts/aarch64-darwin/core/system.nix
@@ -74,6 +74,10 @@
 
   };
 
+  networking = {
+    wakeOnLan.enable = true;
+  };
+
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
