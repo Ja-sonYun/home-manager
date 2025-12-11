a0ea6f3dc0b7df242fbe9dca0ffc8d6c2662aac1
Ja-sonYun <killa30867@gmail.com>
2025-12-11 22:43:35 +0900

    Chore

--- shell/programs/claude/default.nix
+++ shell/programs/claude/default.nix
@@ -19,7 +19,7 @@ let
         "Bash(git show:*)"
         "Bash(ls :*)"
         "Bash(cat :*)"
-        "Base(rg :*)"
+        "Bash(rg:*)"
         "Bash(find :*)"
         "Bash(grep :*)"
         "Bash(tail :*)"
@@ -27,6 +27,10 @@ let
         "Bash(echo :*)"
         "Bash(jq :*)"
         "Bash(yq :*)"
+        "Bash(make:*)"
+        "Bash(nix build:*)"
+        "Bash(nix log:*)"
+        "Bash(nix flake lock:*)"
       ];
       deny = [
         "Read(./.env)"
@@ -34,6 +38,28 @@ let
       ];
     };
     alwaysThinkingEnabled = true;
+    hooks = {
+      Notification = [
+        {
+          matcher = "permission_prompt";
+          hooks = [
+            {
+              type = "command";
+              command = "${pkgs.terminal-notifier}/bin/terminal-notifier -title 'cc'p -message 'Awaiting your input' -sound default";
+            }
+          ];
+        }
+        {
+          matcher = "idle_prompt";
+          hooks = [
+            {
+              type = "command";
+              command = "${pkgs.terminal-notifier}/bin/terminal-notifier -title 'cci' -message 'Awaiting your input' -sound default";
+            }
+          ];
+        }
+      ];
+    };
   };
   mcpServers = {
     github = {
@@ -46,7 +72,11 @@ let
       args = [ ];
       env = { };
       transportType = "stdio";
-      autoApprove = [ ];
+      autoApprove = [
+        "get_file_contents"
+        "search_repositories"
+        "search_code"
+      ];
       disabled = false;
     };
     context7 = {
@@ -57,7 +87,10 @@ let
       args = [ ];
       env = { };
       transportType = "stdio";
-      autoApprove = [ ];
+      autoApprove = [
+        "resolve-library-id"
+        "get-library-docs"
+      ];
       disabled = false;
     };
     playwright = {
@@ -73,7 +106,11 @@ let
       args = [ ];
       env = { };
       transportType = "stdio";
-      autoApprove = [ ];
+      autoApprove = [
+        "read_documentation"
+        "search_documentation"
+        "recommend"
+      ];
       disabled = false;
     };
   };
