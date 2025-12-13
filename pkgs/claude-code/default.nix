46ee9c65af3051f430248acbe7a717e12f0c45c3
Ja-sonYun <killa30867@gmail.com>
2025-12-13 10:09:18 +0900

    Chore

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -7,7 +7,7 @@ pkgs.lib.npm.mkNpmGlobalPackageDerivation {
   inherit pkgs outputHash;
   name = "claude-code";
   packages = [
-    "@anthropic-ai/claude-code@2.0.68"
+    "@anthropic-ai/claude-code@2.0.69"
   ];
   exposedBinaries = [
     "claude"
